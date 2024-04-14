943ef651105cad9543de9f4bce41034437667e6e
4rkal <4rkal@horsefucker.org>
2024-04-14 12:57:55 +0300

    changed some stuff

--- test2.py
+++ test2.py
@@ -12,14 +12,22 @@ def get_bpm():
             return i
     
 def get_pressure():
+    count = 0
     values = []
     for i in m:
         if i != "":
+            i = int(i)
+            i = i /100
             values.append(i)
+            count += 1
+        if count >= 10:
+            return values
+
 session = Session()
 
 bpm = get_bpm()
 bpm = int(bpm)
+bpm = 77
 print(bpm)
 
 if bpm > 90:
@@ -31,7 +39,10 @@ elif bpm < 70:
 piano1 = session.new_part("Piano")
 piano2 = session.new_part("Guitar")
 
-pitches = [64, 66, 71, 73, 74, 66, 64, 73, 71, 66, 74, 73]
+
+pitches = get_pressure()
+pitches.append(bpm)
+# pitches = [64, 66, 71, 73, 74, 66, 64, 73, 71, 66, 74, 73]
 
 
 def piano_part(which_piano):
@@ -40,7 +51,7 @@ def piano_part(which_piano):
             which_piano.play_note(pitch, 1.0, 0.25)
 
 clock1 = session.fork(piano_part, args=(piano1,), initial_tempo=bpm)
-clock2 = session.fork(piano_part, args=(piano2,), initial_tempo=bpm-1)
+clock2 = session.fork(piano_part, args=(piano2,), initial_tempo=bpm-5)
 
 session.start_transcribing(clock=clock1)
 session.wait(10)
